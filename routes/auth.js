41c407d2b5e66c72fe43497b4b1787c2d4fd8ef6
unknown <thrrltk321@gmail.com>
2025-04-01 21:42:37 +0900

    🛠 bcrypt -> bcryptjs 교체 (Render 호환)

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const items = require('../data/items');
 
