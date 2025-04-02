d57a89d285a250a873cc6d62fad9a57b572ae890
youngjae96 <thrrltk321@gmail.com>
2025-04-02 12:42:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ io.on("connection", async (socket) => {
 
 
 
-server.listen(3000, () => {
-  console.log('서버 실행 중: http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`서버 실행 중: http://localhost:${PORT}`);
 });
-
